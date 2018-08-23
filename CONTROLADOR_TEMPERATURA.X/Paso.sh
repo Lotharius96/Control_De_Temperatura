7eb8c3628af66c15a21579b60c468c42df2b7491
Lotharius96 <lmartinezf@correo.udistrital.edu.co>
2018-08-23 18:30:25 -0500

    Modificacion de los LinkerFiles

---
+++ CONTROLADOR_TEMPERATURA.X/Paso.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+Directorio=/home/ultramanjaro/Control/Control_De_Temperatura/CONTROLADOR_TEMPERATURA.X
+Dir_Actual=/home/ultramanjaro/Escritorio/Plantilla/PlantillaBootloaderHIDpic18/MPLABXproject
+declare x
+echo -e "Bienvenido\n"
+for i in $(ls $Dir_Acutal); do
+
+  echo -e "${i}\n"  
+
+   #read x
+    #  if[ $x == "si"] then
+  
+     #  mv $Dir_Actual/$i $Directorio
+      
+     # else 
+
+      #echo "error digitado" 
+      #fi
+done
